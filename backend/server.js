6b5b8026fa34d4b01f119053db2961a6f05c269a
ANTRIKSHKATNA <antrikshkatna03@gmail.com>
2024-07-05 22:20:56 +0530

    user routes controller added

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
-const app=express();
-const PORT=process.env.PORT||5000;
-app.use(express.json()); //to parse req.body
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
-app.use("/api/auth",authRoutes);
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000,()=>{
+app.use(express.json()); //to parse req.body
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.listen(5000, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
-})
+});
