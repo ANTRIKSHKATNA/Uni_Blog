bdea78b5e4f47baf44c162662a87955bae00a19e
ANTRIKSHKATNA <antrikshkatna03@gmail.com>
2024-07-06 18:36:09 +0530

    4thcommit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 app.listen(5000, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
